7781fdef1e86712ec03a30a38314fb427ae52015
Arbnor Rexhepi <r.arbnor@outlook.com>
2022-11-15 16:50:44 +0100

    Expanding cards/ JS completed

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,1 +1,14 @@
+const panels = document.querySelectorAll(".panel"); // It puts all the panel(Clasess) into a nodelist instead of an array
 
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
